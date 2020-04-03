4614825593059429289c01be355277b4fcbab534
Daria <daria359@gmail.com>
2020-04-03 02:00:05 +0200

    Finish the second task

--- kurs-react-od-podstaw/02 - 01 - Przygotowanie pierwszy komponentów/src/App.js
+++ kurs-react-od-podstaw/02 - 01 - Przygotowanie pierwszy komponentów/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import ListWrapper from './components/ListWrapper/ListWrapper';
+import './index.css';
 
 const App = () => (
-    <div>
-        <h1>Hello World</h1>
-    </div>
+    <ListWrapper />
 );
 
 export default App;
--- kurs-react-od-podstaw/02 - 01 - Przygotowanie pierwszy komponentów/src/components/ListItem/ListItem.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ListItem = () => (
-    <li>list item 1</li>
-);
-
-export default ListItem;
